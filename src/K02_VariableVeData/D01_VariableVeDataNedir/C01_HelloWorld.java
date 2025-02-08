85c5b80d8535928b0686e7593f570e1c84b84a8e
bulutluoz <abbulutluoz@gmail.com>
2025-02-08 16:33:26 +0100

    K02 sonu

---
+++ src/K02_VariableVeData/D01_VariableVeDataNedir/C01_HelloWorld.java
@@ -0,0 +1,24 @@
+package K02_VariableVeData.D01_VariableVeDataNedir;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+    // main method'daki keywords sabittir, degistirilemez
+
+    // tek satir yorum eklemek icin // kullaniriz
+
+        System.out.println("Hello World");
+
+     /*
+       birden fazla satir
+       aciklama yazmak isterseniz
+       / ve * kullanarak yazabilirsiniz
+
+      */
+
+
+
+    }
+
+
+}
