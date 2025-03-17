fe45ed801de330e4f1ed9101dd260c87d5e70d63
bulutluoz <abbulutluoz@gmail.com>
2025-03-17 12:25:53 +0100

    Konu13 sonu

--- src/K13_methodOlusturmaVeKullanma/D04_MethodOverloading/C01_Overloading.java
+++ src/K13_methodOlusturmaVeKullanma/D04_MethodOverloading/C01_Overloading.java
@@ -3,24 +3,45 @@ package K13_methodOlusturmaVeKullanma.D04_MethodOverloading;
 public class C01_Overloading {
     public static void main(String[] args) {
 
+        /*
+            Java'da bir class'da ayni isimde method'lar olusturulabilir
+            Buradaki amac bir islemi farkli sekillerde yapabilmektir
 
-
+            Bir class'da ISMI AYNI  farkli method'lar olusturulmasina
+            OVERLOADING denir
+         */
 
 
         /*
             Java'da bir class icerisinde ayni isim ve ayni parametrelere sahip
             birden fazla method OLUSTURULAMAZ.
+            method'daki parametre olarak kullanilan variable isimlerini degistirmek
+            veya method body'sini degistirmek bu sorunu cozmez
 
             Java'da AYNI CLASS icerisinde AYNI ISIMDE method olusturmak istersek
             bu method'larin SIGNATURE'lari farkli olmalidir
 
             Method Signature = method adi + parametrelerin data turleri ve dizilisi
 
-            Bir class'da ISMI AYNI ama SIGNATURE"I farkli method'lar olusturulmasina
-            OVERLOADING denir
-
             Ismi farkli olan method'lar icin OVERLOADING'den bahsedilemez
 
+            overloading olmasi icin signature degismeli ama isim degismemeli
+            bunun yontemi parametreler ile oynamak
+
+            1- parametre sayisini degistirebiliriz
+            2- parametrelerin data turlerini degistirebiliriz
+            3- farkli data turundeki parametrelerin yerlerini degistirebiliriz
+        */
+
+        topla(2); // tek tam sayinin toplami : 4
+        topla(2,3); // Iki tam sayinin toplami : 5
+        topla(3.5,6); // double ve int 2 sayinin toplami : 9.5
+        topla(3,4.5); // int ve double 2 sayinin toplami : 7.5
+
+        //topla(3.2,4.3); // signature'i "topla double double" olan bir method olmadigi icin Java kabul etmez
+
+        // topla("a","b"); // signature'i "topla String String" olan bir method olmadigi icin Java kabul etmez
+        /*
             Bir method call'un calismasi icin
             method call'da yazdigimiz argumentler ile
             method'daki parametreler uyumlu olmalidir
@@ -29,33 +50,32 @@ public class C01_Overloading {
             CTE olusur, Java kodu kirmizi cizer ve kod calismaz
          */
 
-        // topla("ali","veli");
+
+
 
     }
 
-    public static void topla(int sayi1 , int sayi2){ // topla int int
-        System.out.println("Iki int sayinin toplami : " + (sayi1+sayi2));
-    } // topla int int
+    public static void topla(int a , int b){
+        System.out.println("Iki tam sayinin toplami : " + (a+b));
+    } // method signature : topla int int
 
-    public static void carpim(int sayi1 , int sayi2){ // topla int int
-        System.out.println("Iki int sayinin toplami : " + (sayi1*sayi2));
-    } // carpim int int
+    // 'topla(int, int)' is already defined in 'C01_Overloading'
+//    public static void topla(int sayi1 , int sayi2){
+//        System.out.println("Iki int sayinin toplami : " + (sayi1+sayi2));
+//    } // method signature : topla int int
 
-    public static void topla(int sayi1 , int sayi2, int sayi3){ // topla int int
-        System.out.println("Iki int sayinin toplami : " + (sayi1+sayi2));
-    } // topla int int int
+    public static void topla(int a){
+        System.out.println("tek tam sayinin toplami : " + (a+a));
+    } // method signature : topla int
 
-    public static void topla(int a ){
-        System.out.println("Iki int sayinin toplami : " + (a+a));
-    }// topla int
+    public static void topla(double a , int b){
+        System.out.println("double ve int 2 sayinin toplami : " + (a+b));
+    } // method signature : topla double int
 
-    public static void topla(double sayi1 , int sayi2){
-        System.out.println("Iki int sayinin toplami : " + (sayi1+sayi2));
-    }// topla double int
+    public static void topla(int a , double b){
+        System.out.println("int ve double 2 sayinin toplami : " + (a+b));
+    } // method signature : topla int double
 
-    public static void topla(int sayi1 , double sayi2){
-        System.out.println("Iki int sayinin toplami : " + (sayi1+sayi2));
-    }// topla int double
 
 
 }
--- src/K13_methodOlusturmaVeKullanma/D04_MethodOverloading/C02_Overloading.java
+++ src/K13_methodOlusturmaVeKullanma/D04_MethodOverloading/C02_Overloading.java
@@ -3,34 +3,68 @@ package K13_methodOlusturmaVeKullanma.D04_MethodOverloading;
 public class C02_Overloading {
 
     public static void main(String[] args) {
+     /*
+        Java method call'daki argumentler ile
+        method'daki parametreleri kontrol eder
+        Eger signature'a %100 uyumlu olan bir method bulursa onu calistirir
+     */
 
-
+        topla(3,4); // topla int int 1 ==> 3+4+1 ==> 8
+        topla(2.4,3.25); // topla double double 2 ==> 2.4+3.25+2 ==> 7.65
+        topla(5,4.5); // topla int double 3 ==> 5+4.5+3 ==> 12.5
 
      /*
         Java method call'daki signature'a %100 uyumlu olan bir method bulamazsa
         auto-widening kullanarak calistirabilecegi method var mi diye kontrol eder
         ve varsa calistirir
      */
+        topla(3.4f, 4.2f); // topla float float
+        // Iki double'in toplami : 9.599999904632568
+        // 1.ve 3.method float'i kabul etmeyecegi icin 2.method calisti
 
-        topla(3,5); // topla int int ==> Iki int'in toplami : 8
+        topla(3L, 4); // topla long int
+        // Iki double'in toplami : 9.0
 
-        topla(3.4,2.3); // topla double double ==> Iki double'in toplami : 5.699999999999999
 
-        topla(2.4, 5); // topla double int ==> Iki double'in toplami : 7.4
 
-        topla('a',6); // topla char int ==> Iki int'in toplami : 103
+     /*
+        %100 uyumlu method olmadigi durumda,
+        auto-widening kullanarak calistirabilecegi
+        birden fazla method varsa
+        en az casting yaparak calistirabilecegi method'u tercih eder
+     */
+
+        topla(4,5L);  // topla int long
+        // int ve double'in toplami : 12.0
+
+        topla(3,4.1F);
+        // int ve double'in toplami : 10.099999904632568
+
+
+        topla('a','b'); // topla char char
+        // Iki int'in toplami : 196
 
     }
 
-    public static void topla(int a, int b){ // topla int int
 
-        System.out.println("Iki int'in toplami : " + (a+b));
-    }
+    public static void topla(int a, int b){
 
-    public static void topla(double a, double b){ // topla double double
+        System.out.println("Iki int'in toplami : " + (a+b+1));
+    } // topla int int   ==> 1
 
-        System.out.println("Iki double'in toplami : " + (a+b));
-    }
+
+
+    public static void topla(double a, double b){
+
+        System.out.println("Iki double'in toplami : " + (a+b+2));
+    } // topla double double ==>2
+
+
+
+    public static void topla(int a, double b){
+
+        System.out.println("int ve double'in toplami : " + (a+b+3));
+    } // topla int double ==>3
 
 
 
