dd80101f04f5acc4e93377c1b04226dbf801939c
bulutluoz <abbulutluoz@gmail.com>
2025-03-19 12:33:01 +0100

    Konu15 sonu

---
+++ src/K15_doWhileLoop/D01_DoWhileLoopVsWhileLoop/C01_whileLoopVsDoWhileLoop.java
@@ -0,0 +1,32 @@
+package K15_doWhileLoop.D01_DoWhileLoopVsWhileLoop;
+
+public class C01_whileLoopVsDoWhileLoop {
+
+    public static void main(String[] args) {
+
+        /*
+            While loop da condition kontrolu
+            loop body'den daha fazla calisir
+
+            do-while loop'da
+            condition kontrol sayisi ile
+            loop body calisma sayisi aynidir.
+         */
+
+
+        // 1'den 5'e kadar(5 haric) olan sayilari yazdirin
+
+
+        // while loop
+
+
+        // do while loop ile
+
+
+
+
+
+
+
+    }
+}
---
+++ src/K15_doWhileLoop/D01_DoWhileLoopVsWhileLoop/C02_whileLoopVsDoWhileLoop.java
@@ -0,0 +1,43 @@
+package K15_doWhileLoop.D01_DoWhileLoopVsWhileLoop;
+
+import java.util.Scanner;
+
+public class C02_whileLoopVsDoWhileLoop {
+
+    public static void main(String[] args) {
+
+        /*
+            while loop'un en buyuk eksisi
+            kullanicidan deger alinan gorevlerde
+            loop calismadan once kod yazan kisinin atayacagi degerin ONEMLI olmasidir
+
+            Cunku kod yazan kisi ilk deger atamasini yanlis yaparsa
+            LOOP BODY hic calismayabilir.
+         */
+
+        // Soru 4- Kullanicidan toplanmak uzere pozitif tamsayilar isteyin
+        //         Kullaniciya bitirmek istediginde 0'a basmasini soyleyin
+        //         Kullanici bitirmek istediginde toplam kac adet pozitif tam sayi girdigini
+        //         ve bunlarin toplaminin kac oldugunu yazdirin
+        //         Kullanici negatif sayi girerse "negatif sayi kullanamazsiniz " yazdirin
+        //         bu negatif sayiyi, sayi adedine ve toplama eklemeyin
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sayi=0;
+        int sayiToplami = 0;
+        int sayac = 0 ;
+
+
+        // while loop ile yapalim
+
+
+
+        // aynisini do while ile yapalim
+
+
+
+
+
+    }
+}
