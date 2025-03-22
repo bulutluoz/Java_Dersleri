12cfe1ef8c0afa907f0259b6017c4e6d0500d2c6
bulutluoz <abbulutluoz@gmail.com>
2025-03-22 13:48:17 +0100

    Konu16 sonu

--- src/K16_scope/D03_ClassLevelVariableKullanimi/Hastane.java
+++ src/K16_scope/D03_ClassLevelVariableKullanimi/Hastane.java
@@ -4,62 +4,10 @@
         static String hastaneIsmi  = "Yildiz Hastanesi";
         static String hastaneAdresi = "Yenimahalle";
         static String bashekim = "Doktor Ali";
+
         String perIsmi = "Isim atanmadi";
         String perAdres = "Adres atanmadi";
         String perTel ="Telefon atanmadi";
 
-        public static void main(String[] args) {
-            /*
-                Bir class'dan bir obje olusturuldugunda
-                Java ait oldugu class'daki Instance variable'larin bir kopyasini alir
-                ve olusturulan objeye yapistirir
-
-                Obje olusturulduktan sonra objeye ait bir bilgi kullanildiginda
-                Java class'daki instance variable'a degil
-                Obje'ye yapistirdigi kopya variable'a gider
-
-                Static variable'lar objeye yapistirilmaz
-                Onlar class'a aittir
-             */
-
-            Hastane per1 = new Hastane();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
 
 }
--- src/K16_scope/D03_ClassLevelVariableKullanimi/Runner.java
+++ src/K16_scope/D03_ClassLevelVariableKullanimi/Runner.java
@@ -6,6 +6,52 @@ public class Runner {
 
     public static void main(String[] args) {
 
+        Hastane per1 = new Hastane();
+
+        System.out.println(per1.perAdres); // adres atanmadi
+        System.out.println(per1.perIsmi); // isim atanmadi
+
+        System.out.println(per1.hastaneIsmi); // Yildiz Hastanesi
+
+
+        per1.perIsmi = "Kemal";
+        per1.perAdres = "cankaya";
+        per1.perTel = "5323232341";
+
+        System.out.println(per1.perAdres); // cankaya
+        System.out.println(per1.perIsmi); // Kemal
+
+        System.out.println(per1.hastaneIsmi); // Yildiz Hastanesi
+
+
+        Hastane per2 = new Hastane();
+
+
+        System.out.println(per2.perIsmi); // Isim atanmadi
+        System.out.println(per1.perIsmi); // Kemal
+
+        System.out.println(per2.perAdres); // Adres atanmadi
+        System.out.println(per1.perAdres); // 532.....
+
+        System.out.println(per1.bashekim); // Doktor Ali
+        System.out.println(per2.bashekim); // Doktor Ali
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
