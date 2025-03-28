67521a55c2e8bd94db0318147be166abcd355203
bulutluoz <abbulutluoz@gmail.com>
2025-03-28 10:32:59 +0100

    day33 sonu

--- src/K17_arrays/D05_multiDimensionalArrays/C01_ElemanlaraUlasma.java
+++ src/K17_arrays/D05_multiDimensionalArrays/C01_ElemanlaraUlasma.java
@@ -19,21 +19,52 @@ public class C01_ElemanlaraUlasma {
 
         // 1-  Multidimentional array'de bir elemani yazdirmak istersek
 
+        System.out.println(arr[0][0]); // 2
+
+        // 7'yi yazdirin
+        System.out.println(arr[0][2]); // 7
+
+        // 5'yi yazdirin
+        System.out.println(arr[1][1]); // 5
+
+        // 4'yi yazdirin
+        System.out.println(arr[2][0]); // 4
+
+        // 8'yi yazdirin
+        System.out.println(arr[3][4]); // 8
+
 
         // 2- Multidimentional bir array'de inner array'i yazdirmak istersek
-
+        //    int[][] arr = { {2,3,7},{3,5},{4},{1,2,3,6,8} };
 
 
         //    arr[3][1] 'ini yazdir
+        System.out.println(arr[3][1]); // 2
 
 
+        //    arr[1] 'i yazdir
 
-        // arr[1] 'i yazdir
+        System.out.println(arr[1]);
+        // arr[1] icinde 3 ve 5'in oldugu bir array {3,5}
+        // bir array'i direkt sout icerisinde yazdirirsak bize referansini yazdirir
+        // [I@2752f6e2
+
+        System.out.println(Arrays.toString(arr[1])); // [3, 5]
+
+        System.out.println(Arrays.toString(arr[2])); // [4]
+
 
 
 
         // 3- Multidimentional bir array'i yazdirmak istersek
-        // { {2,3,7},{3,5},{4},{1,2,3,6,8} }
+        //    { {2,3,7},{3,5},{4},{1,2,3,6,8} }
+
+        System.out.println(arr); // [[I@e580929
+
+        System.out.println(Arrays.toString(arr)); // [[I@1cd072a9, [I@2752f6e2, [I@7c75222b, [I@4c203ea1]
+
+        System.out.println(Arrays.deepToString(arr)); // [[2, 3, 7], [3, 5], [4], [1, 2, 3, 6, 8]]
+
 
 
 
--- src/K17_arrays/D05_multiDimensionalArrays/C02_TumElemanlariToplama.java
+++ src/K17_arrays/D05_multiDimensionalArrays/C02_TumElemanlariToplama.java
@@ -6,17 +6,50 @@ public class C02_TumElemanlariToplama {
 
         int[] sayilar = {3, 6, 9, 1};
 
-        // array'in tum elementlerinin toplamini yazdirin
+        // sayilar array'inin tum elementlerinin toplamini yazdirin
+
+        int toplam =0 ;
+
+        for (int i = 0; i < sayilar.length ; i++) {
+            toplam += sayilar[i];
+        }
+
+
+        System.out.println("sayilar array'indeki elementlerin toplami : " + toplam); // 19
 
 
 
 
-
-
-
+        toplam =0;
         int[][] arr = {{2, 3, 7}, {3, 5}, {4}, {1, 2, 3, 6, 8}};
 
 
+        for (int i = 0; i < arr.length ; i++) { // outer array'in elemani olan inner array'leri getirir
+
+            for (int j = 0; j < arr[i].length; j++) { // inner array icindeki elementleri getirir
+                                                      // her inner array'de farkli sayida element olacagi icin
+                                                      // inner loop'un length'ine kadar gitmeli
+
+                toplam += arr[i][j];
+            }
+
+        }
+
+        System.out.println("arr'deki sayilarin toplami : " + toplam); // 44
+
+
+
+
+        // arr'deki tum elementleri yanyana yazdirin
+        // {{2, 3, 7}, {3, 5}, {4}, {1, 2, 3, 6, 8}};
+
+        for (int i = 0; i < arr.length ; i++) { // outer array'i kontrol eder
+            for (int j = 0; j <arr[i].length ; j++) { // inner array'leri kontrol eder
+
+                System.out.print(arr[i][j] + " ");
+            }
+
+        }
 
 
 
